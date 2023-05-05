90870aea98090c3017bce5ce028d188a9c43dc71
Marco Valdez <antem.marco@gmail.com>
2023-05-04 19:01:52 -0600

    feat: added auth features

---
+++ features/auth/authSlice.js
@@ -0,0 +1,32 @@
+import axios from "axios"
+
+const API_URL = "https://red-beautiful-lobster.cyclic.app/api/users"
+
+// Registrar usuario
+
+const register = async(userData)=> {
+    const response = await axios.post(API_URL, userData);
+    return response.data;
+}
+
+// Login 
+
+const login = async (userData) => {
+    const response = await axios.post(`${API_URL}/login`, userData)
+    if(response.data){
+        localStorage.setItem("user", JSON.stringify(response.data))
+    }
+    return response.data
+}
+
+const logout = () => {
+    localStorage.removeItem("user");
+}
+
+const authService = {
+    register,
+    logout,
+    login,
+};
+
+export default authService;
