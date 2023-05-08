c2d9f2df479b815ec09cfeca8a18aa4779e7c596
Marco Valdez <antem.marco@gmail.com>
2023-05-08 10:18:01 -0600

    feat: ended basic movieSlice

--- src/features/movies/movieSlice.js
+++ src/features/movies/movieSlice.js
@@ -19,3 +19,80 @@ export const createMovie = createAsyncThunk("movies/create", async(movieData, th
         return thunkAPI.rejectWithValue(message);
     }
 })
+
+// Obtener todas las peliculas
+export const getMovies = createAsyncThunk("movies/getAll", async(_, thunkAPI)=>{
+    try {
+        const token = thunkAPI.getState().auth.user.token
+        return await movieService.getMovies(token)
+    } catch (error) {
+        const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
+        return thunkAPI.rejectWithValue(message)
+    }
+})
+
+// Eliminar una pelicula
+export const deleteMovie = createAsyncThunk("movies/detele", async(id, thunkAPI) => {
+    try {
+        const token = thunkAPI.getState().auth.user.token
+        return await movieService.deleteMovie(id, token)
+    } catch (error) {
+        const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
+        return thunkAPI.rejectWithValue(message)
+        
+    }
+})
+
+// Crear Slice
+export const movieSlice = createSlice({
+    name: "movie",
+    initialState,
+    reducers: {
+        reset: (state)=> initialState
+    },
+    extraReducers: (builder) => {
+        builder
+        .addCase(createMovie.pending, (state)=>{
+            state.isLoading = true
+        })
+        .addCase(createMovie.fulfilled, (state, action) => {
+            state.isLoading = false
+            state.isSuccess = true
+            state.tareas.push(action.payload)
+        })
+        .addCase(createMovie.rejected, (state, action) => {
+            state.isLoading = false
+            state.isError = true
+            state.message = action.payload
+        })
+        .addCase(getMovies.pending, (state)=>{
+            state.isLoading = true
+        })
+        .addCase(getMovies.fulfilled, (state, action) => {
+            state.isLoading = false
+            state.isSuccess = true
+            state.tareas.push(action.payload)
+        })
+        .addCase(getMovies.rejected, (state, action) => {
+            state.isLoading = false
+            state.isError = true
+            state.message = action.payload
+        })
+        .addCase(deleteMovie.pending, (state)=>{
+            state.isLoading = true
+        })
+        .addCase(deleteMovie.fulfilled, (state, action) => {
+            state.isLoading = false
+            state.isSuccess = true
+            state.tareas.push(action.payload)
+        })
+        .addCase(deleteMovie.rejected, (state, action) => {
+            state.isLoading = false
+            state.isError = true
+            state.message = action.payload
+        })
+    }
+})
+
+export const { reset } = movieSlice.actions
+export default movieSlice.reducer
