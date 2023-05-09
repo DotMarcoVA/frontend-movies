b6c9fcd5dbda7253f0501291a9f9f6f5971e5ae1
Marco Valdez <antem.marco@gmail.com>
2023-05-09 12:20:59 -0600

    feat: login completed with an error at success

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,65 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import { useNavigate, Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { toast } from "react-toastify";
+import { login, reset } from "../features/auth/authSlice";
 
 const Login = () => {
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+    });
+
+    const { email, password } = formData;
+
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const { user, isLoading, isError, isSuccess, message } = useSelector(
+        (state) => state.auth
+    );
+
+    useEffect(() => {
+        if (isError) {
+            toast.error(message);
+        }
+        if (isSuccess) {
+            navigate("/dashboard");
+        }
+        dispatch(reset());
+    }, [user, isError, isSuccess, message, navigate, dispatch]);
+
+    const onChange = (e) => {
+        setFormData((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value,
+        }));
+    };
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        const userData = { email, password };
+        dispatch(login(userData));
+    };
+
+    if (isLoading) {
+        return; // Aqui colocar un Loading Screen
+    }
+
     return (
-        <form className="box p-6 m-6">
+        <form className="box p-6 m-6" onSubmit={onSubmit}>
             <div className="field">
                 <label className="label">Email</label>
                 <div className="control">
                     <input
                         className="input"
                         type="email"
+                        name="email"
+                        id="email"
+                        value={email}
                         placeholder="e.g. alex@example.com"
+                        onChange={onChange}
                     />
                 </div>
             </div>
@@ -21,12 +70,18 @@ const Login = () => {
                     <input
                         className="input"
                         type="password"
+                        name="password"
+                        id="password"
+                        value={password}
                         placeholder="********"
+                        onChange={onChange}
                     />
                 </div>
             </div>
 
-            <button className="button is-black">Sign in</button>
+            <button type="submit" className="button is-black">
+                Sign in
+            </button>
             <button className="button is-white is-text">
                 <Link to="/register">Create Account</Link>
             </button>
