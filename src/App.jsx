4db35f2f2d5c2e62585681ffe3b2dd6e558cdad0
Marco Valdez <antem.marco@gmail.com>
2023-05-08 20:02:07 -0600

    feat: Added main pages with no content for structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
-import './App.css'
+import "./App.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
-  return (
-    <>
-      <Router>
-        <div>
-        <Routes>
-          <Route/>
-        </Routes>
-        </div>
-      </Router>
-      <ToastContainer/>
-    </>
-  )
+    return (
+        <>
+            <Router>
+                <div>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/dashboard" element={<Dashboard />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/register" element={<Register />} />
+                    </Routes>
+                </div>
+            </Router>
+            <ToastContainer />
+        </>
+    );
 }
 
-export default App
+export default App;
