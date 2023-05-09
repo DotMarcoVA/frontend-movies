9196cf5f97f6a8c0e45e16c063240abc299740d2
Marco Valdez <antem.marco@gmail.com>
2023-05-09 12:45:16 -0600

    feat: register functionality with error at end

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,16 +1,73 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { useNavigate, Link } from "react-router-dom";
+import { toast } from "react-toastify";
+import { register, reset } from "../features/auth/authSlice";
 
 const Register = () => {
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        password: "",
+        password2: "",
+    });
+
+    const { name, email, password, password2 } = formData;
+
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const { user, isLoading, isError, isSuccess, message } = useSelector(
+        (state) => state.auth
+    );
+
+    useEffect(() => {
+        if (isError) {
+            toast.error(message);
+        }
+
+        if (isSuccess) {
+            navigate("/login");
+        }
+
+        dispatch(reset());
+    }, [user, isError, isSuccess, message, navigate, dispatch]);
+
+    const onChange = (e) => {
+        setFormData((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value,
+        }));
+    };
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        if (password !== password2) {
+            toast.error("Las contraseñas con coinciden");
+        } else {
+            const userData = { name, email, password };
+            dispatch(register(userData));
+        }
+    };
+
+    if (isLoading) {
+        return; // Colocar aqui algo de carga
+    }
+
     return (
-        <form className="box p-6 m-6">
+        <form className="box p-6 m-6" onSubmit={onSubmit}>
             <div className="field">
                 <label className="label">Name</label>
                 <div className="control">
                     <input
                         className="input"
                         type="text"
+                        name="name"
+                        id="name"
+                        value={name}
                         placeholder="Zack Herleman"
+                        onChange={onChange}
                     />
                 </div>
             </div>
@@ -21,7 +78,11 @@ const Register = () => {
                     <input
                         className="input"
                         type="email"
+                        name="email"
+                        id="email"
+                        value={email}
                         placeholder="alex@example.com"
+                        onChange={onChange}
                     />
                 </div>
             </div>
@@ -32,12 +93,33 @@ const Register = () => {
                     <input
                         className="input"
                         type="password"
+                        name="password"
+                        id="password"
+                        value={password}
                         placeholder="********"
+                        onChange={onChange}
                     />
                 </div>
             </div>
 
-            <button className="button is-black">Create Account</button>
+            <div className="field">
+                <label className="label">Confirm Password</label>
+                <div className="control">
+                    <input
+                        className="input"
+                        type="password"
+                        name="password2"
+                        id="password2"
+                        value={password2}
+                        placeholder="********"
+                        onChange={onChange}
+                    />
+                </div>
+            </div>
+
+            <button type="submit" className="button is-black">
+                Create Account
+            </button>
         </form>
     );
 };
