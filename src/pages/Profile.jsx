b812696708ae5e5c2bb9371b97871a5d245d1c24
Marco Valdez <antem.marco@gmail.com>
2023-05-15 13:22:41 -0600

    feat: receiving data on profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,9 +4,19 @@ import { useSelector, useDispatch } from "react-redux";
 import {} from "../features/auth/authSlice";
 
 const Profile = () => {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const { user } = useSelector((state) => state.auth);
+    const { name, email, isAdmin } = user;
+    console.log(name, email, isAdmin);
+
     return (
         <>
-            <div>profile</div>
+            <div>
+                {name}
+                {email}
+                {isAdmin}
+            </div>
         </>
     );
 };
